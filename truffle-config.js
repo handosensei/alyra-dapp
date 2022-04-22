f67a90d78023724ea3f294d1a37d71d9844d411f
Raldo CHEA <raldo.chea@gmail.com>
2022-04-23 00:20:03 +0200

    Add module react-router-dom

--- truffle-config.js
+++ truffle-config.js
@@ -9,7 +9,7 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,            // Standard Ethereum port (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*"       // Any network (default: none)
     },
     ropsten: {
