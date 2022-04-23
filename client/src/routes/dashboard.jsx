d286d2b2c3267e868b6ad2d195cf81691fc47856
Raldo CHEA <raldo.chea@gmail.com>
2022-04-23 02:10:45 +0200

    Init routes
    Set routes

---
+++ client/src/routes/dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Dashboard() {
+    return (
+        <main style={{ padding: "1rem 0" }}>
+            <h2>Dashboard</h2>
+        </main>
+    );
+}
+
+
---
+++ client/src/routes/homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Homepage() {
+    return (
+        <main style={{ padding: "1rem 0" }}>
+            <h2>Homepage</h2>
+        </main>
+    );
+}
+
+
